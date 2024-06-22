6edb7c41bb16e9d457cb99be3faf5b203bd57174
mauriciomr <mauriciomr@gmail.com>
2024-06-22 16:55:36 -0400

    added code for thir questions and order the code

--- src/q3_time.py
+++ src/q3_time.py
@@ -1,4 +1,22 @@
+import pandas as pd
+from collections import Counter
 from typing import List, Tuple
 
+def extract_mentions(users: List[dict]) -> List[str]:
+    """get usernme from mentions"""
+    return [user['username'] for user in users]
+
 def q3_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    # load file in panda dataframe
+    tweets_df = pd.read_json(file_path, lines=True)
+
+    #  get the mentions in every twit
+    all_mentions = [mention for mentions in tweets_df['mentionedUsers'].dropna() for mention in extract_mentions(mentions)]
+
+    # count mentions
+    mention_counts = Counter(all_mentions)
+
+    # get top ten mentioned users
+    top_ten_user_mentions = mention_counts.most_common(10)
+
+    return top_ten_user_mentions
